a62c56107e57c82045e924d096cd814a5aafb138
cloudheadboatman <adam.boatman@gmail.com>
2017-08-26 18:50:03 +0100

    styling and basic buttons added

--- public/javascripts/flavourSeeker.js
+++ public/javascripts/flavourSeeker.js
@@ -1,5 +1,23 @@
 $(document).ready(function(){
-  $('body').append('helloooooo!');
+  var searchParams = [];
+  $('.flavour-form').submit(function (event){
+    event.preventDefault();
+    var item = $('.flavour-form-input').val();
+    console.log(item);
+    searchParams.push(item);
+    $('.search-terms')
+        .append('<button></button>')
+        .addClass('search-item')
+        .text(item);
+    $('.flavour-form-input').val('');
+    $('.flavour-form-input').focus();
+    console.log(searchParams);
+    $('.')
+  })
+  // Display results of input in results div
+  // Push previous search to array
+  // Repeat process with second search
+  // Add section to results div showing combined flavour matches
 
 
 });
