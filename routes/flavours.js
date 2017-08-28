3575d80db7784fc255ade15114a4d8c369bad6a5
Lawrence Hunt <lawrence.hunt@hotmail.com>
2017-08-28 23:37:27 +0100

    using underscore intersection method

--- routes/flavours.js
+++ routes/flavours.js
@@ -1,38 +1,6 @@
-//
-//
-// module.exports = {
-//   index: function(req, res) {
-//       res.render('main', { title: 'Flavourise' });
-//   },
-//   update: function(req, res) {
-//     var flavours = require('../flavours/flavourIndex');
-//     var searchTerms = req.body.flavours;
-//     var searchLength = searchTerms.length;
-//     var searchResponse = []; // results array passed to the client side
-//     var holderArray = []; // For non-matched flavours when using more than two search items
-//
-//     searchResponse = flavours[searchTerms[0]];
-//
-//     if (searchLength > 1) {
-//       for (i = 1; i < searchLength; i++) {
-//         var matchArray = flavours[searchTerms[i]];
-//         j = 0;
-//         while (j < searchResponse.length) {
-//           if (matchArray.indexOf(searchResponse[j]) < 0) {
-//             searchResponse.splice(j, 1);
-//           } else {
-//             j++;
-//           }
-//         }
-//       }
-//     }
-//
-//   res.send(searchResponse);
-//   }
-//
-// };
 
 var flavours = require('../flavours/flavourIndex');
+var _ = require("underscore");
 
 module.exports = {
   index: function(req, res) {
@@ -43,19 +11,13 @@ module.exports = {
     var searchTerms = req.body.flavours;  // an array of flavour requests.
     var searchResponse = [];              // our returned array of results.
 
+    // push all results into the array.
     for (i = 0; i < searchTerms.length; i++) {
       searchResponse.push(flavours[searchTerms[i]]);
     };
 
-    searchResponse = searchResponse.reduce(function(a, b){    // flatten the array.
-      return a.concat(b);
-    }, []);
-
-    console.log('searchResponse: ', searchResponse);
-    // remove duplicates from the search array.
-    searchResponse = searchResponse.filter(function(item) {
-      return searchResponse.indexOf(item) == searchResponse.lastIndexOf(item);      
-    });
+    // find common elements between all arrays
+    searchResponse = _.intersection.apply(_, searchResponse);
 
     res.send(searchResponse);
   }
