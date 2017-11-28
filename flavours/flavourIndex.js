901da9124a836184d1a148b2d97bdb9c5212d210
cloudheadboatman <adam.boatman@gmail.com>
2017-11-28 12:02:01 +0000

    update flavours

--- flavours/flavourIndex.js
+++ flavours/flavourIndex.js
@@ -2,8 +2,8 @@
 
 module.exports = {
   chocolate: ["almond", "anise", "apricot", "avocado", "bacon", "banana", "beetroot", "blackcurrant", "black pudding", "cardamom", "cauliflower", "cherry", "chestnut", "chilli", "cinnamon", "coconut", "coffee", "fig", "ginger", "goats cheese", "hazelnut", "lemon", "lime", "mint", "nutmeg", "orange", "peanut", "pear", "pineapple", "raspberry", "rose", "rosemary", "thyme", "tomato", "vanilla", "walnut", "watermelon", "white chocolate"],
-  almond: ["cinnamon", "poo", "death-crab", "chilli", "satan", "nutsack"],
-  cinnamon: ["almond", "chilli", "poo", "oil", "parsley", "tarragon", "nutsack"],
+  almond: ["cinnamon", "toffee", "death-crab", "chilli", "bill bailey", "beer"],
+  cinnamon: ["almond", "chilli", "toffee", "oil", "parsley", "tarragon", "death-crab"],
   coffee: ["almond", "avocado", "banana", "beef", "blackcurrant", "cardamom", "cherry", "chocolate", "cinnamon", "clove", "coriander seed", "ginger", "goats cheese", "hazelnut", "orange", "rose", "vanilla", "walnut", "white chocolate"],
   peanut: ["apple", "asparagus", "banana", "beef", "blackcurrent", "broccoli", "carrot", "celery", "chicken", "chilli", "chocolate", "cinnamon", "coconut", "coriander leaf", "cucumber", "grape", "lamb", "lime", "mint", "pork", "potato", "shellfish", "tomato", "vanilla"],
   chicken: ["almond", "anise", "avocado", "bacon", "banana", "basil", "bell pepper", "blue cheese", "cabbage", "caviar", "celery", "chesnut", "chilli", "coconut", "coriander leag", "egg", "garlic", "grape", "hard cheese", "hazelnut", "lemon", "lime", "mushroom", "onion", "oyster", "parsnip", "pea", "peanut", "pear", "potato", "rose", "saffron", "sage", "shellfish", "thyme", "tomato", "truffle", "walnut", "watercress"],
